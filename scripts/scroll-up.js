1a8fc3115d2caec37a87912d15d1bbd8ec25b91d
Shkliarova <shklyarovaalisa@gmail.com>
2024-10-21 19:26:56 +0300

    add scroll-up

--- scripts/scroll-up.js
+++ scripts/scroll-up.js
@@ -1,24 +1,19 @@
-import { throttle } from "./func-throttle";
-
 const scrollUpButton = document.querySelector('.scroll-up-btn');
 
-scrollUpButton.addEventListener("click", scrollToTop);
+window.addEventListener("scroll", () => {
+    
+    if (document.documentElement.scrollTop > 100) {
+        scrollUpButton.classList.add('show'); 
+        scrollUpButton.classList.remove('hide');
+    } else {
+        scrollUpButton.classList.remove('show'); 
+        scrollUpButton.classList.add('hide');
+    }
+});
 
-const throttledScrollHandler = throttle(() => {
-  if (document.documentElement.scrollTop > 100) {
-    scrollUpButton.classList.add('show');
-    scrollUpButton.classList.remove('hide');
-  } else {
-    scrollUpButton.classList.add('hide');
-    scrollUpButton.classList.remove('show');
-  }
-}, 350);
-
-window.addEventListener("scroll", throttledScrollHandler);
-
-function scrollToTop() {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth',
-  });
-}
+scrollUpButton.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    });
+});
